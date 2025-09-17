596c94781408db36e4bc84dcbe1d129ec99611b0
Ury Dol3wsky <dol3wsky@gmail.com>
2025-09-17 04:13:32 +0300

    upload_file_manager

---
+++ app/utils/upload_files.py
@@ -0,0 +1,19 @@
+import os
+import aiofiles
+from fastapi import UploadFile
+from config.logging import app_logger as logger
+
+async def save_upload_media(file: UploadFile):
+
+    #upload_folder = "static"
+    upload_folder = f"docs/image" #дл докер контейнера
+    # Создаем директорию для картинки, если ее не
+    if not os.path.exists(upload_folder):
+        os.makedirs(upload_folder)
+    full_path = os.path.join(upload_folder, file.filename)
+    # Сохраняем изображение
+    async with aiofiles.open(full_path, mode="wb") as buffer:
+        await buffer.write(file.file.read())
+    logger.info(f"Uploaded file saved at {full_path}")
+
+    return file.filename
